a7d17940398f920a14687804364acf20edadc836
angelop47 <69943997+angelop47@users.noreply.github.com>
2024-09-10 13:39:33 -0300

    Add files via upload

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Proyecto Colonia';
+export const SITE_DESCRIPTION = 'Movimiento político en el Departamento de Colonia.';
